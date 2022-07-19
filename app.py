42ace385da74183c59123d99be1c0b8b5564adec
Akhil <akhilssmec@gmail.com>
2022-07-19 08:26:41 +0000

    changes

--- app.py
+++ app.py
@@ -6,45 +6,44 @@ from dash import dcc
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
+artist=['Roddy Rich', 'Lil Nas X', 'Eminem', 'Olivia']
+song_dur=[3.21, 2.09, 2.89, 3.05]
+title_len=[2, 2, 5, 3]
+color1='blue'
+color2='black'
+mytitle='Artist song durations & number of titles'
 
-label1='IBU'
-label2='ABV'
+label1='duration'
+label2='number of titles'
 
 ########### Set up the chart
 
-def make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle):
-    bitterness = go.Bar(
-        x=beers,
-        y=ibu_values,
+def make_that_cool_barchart(artist, song_dur, title_len, color1, color2, mytitle):
+    dur = go.Bar(
+        x=artist,
+        y=song_dur,
         name=label1,
         marker={'color':color1}
     )
-    alcohol = go.Bar(
-        x=beers,
-        y=abv_values,
+    leng = go.Bar(
+        x=artist,
+        y=title_len,
         name=label2,
         marker={'color':color2}
     )
 
-    beer_data = [bitterness, alcohol]
-    beer_layout = go.Layout(
+    artist_data = [dur, leng]
+    artist_layout = go.Layout(
         barmode='group',
         title = mytitle
     )
 
-    beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-    return beer_fig
+    return go.Figure(data=artist_data, layout=artist_layout)
 
 
 ######### Run the function #######
 
 if __name__ == '__main__':
-    fig = make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle)
+    fig = make_that_cool_barchart(artist, song_dur, title_len, color1, color2, mytitle)
     fig.write_html('docs/barchart.html')
     print('We successfully updated the barchart!')
